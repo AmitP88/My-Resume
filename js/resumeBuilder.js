3ddeca8372ddb8cdcda34ad3ace81f626c3c1be3
Amit Patel <amitpatel_7@msn.com>
2016-04-13 14:20:11 -0700

    Updated Work Experience and Projects fields

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,14 @@
 var bio = {
 	"name": "Amit Patel",
-	"role": "Web Developer",
+	"role": "Front End Web Developer",
 	"contacts":{
 		"mobile": "559-347-7496",
 		"email": "amitpatel_7@msn.com",
 		"github": "AmitP88",
-		"location": "Fresno,CA"
+		"location": "Visalia, CA"
 		},
 	"welcomeMessage": "Hello! Welcome to my resume!",
-	"age": 26,
+	"age": 27,
 	"skills": ["HTML", "CSS", "Javascript", "Git version control"],
 	"bioPic": ["images/Amit.jpg"]
 };
@@ -35,25 +35,32 @@ var education =  {
 var work = {
 	"jobs": [
 		{
+			"employer": "Ventura Coastal LLC.",
+			"title": "Lab Technician II",
+			"location": "Visalia, CA",
+		 	"date": "11/15-Present",
+		 	"description": "Extract and analyze citrus juice for Brix, pH, percent acidity, and percent oil."
+		},
+		{
 			"employer": "Deerpoint Group Inc.",
 			"title": "Lab Technician",
 			"location": "Fresno, CA",
 		 	"date": "4/15-5/15",
-		 	"description": "Performed various testing on plant and water samples such as pH, titration, EC, Flow Injection Analysis, and microwave digestion reactions."
+		 	"description": "Conducted microwave reactions on dried plant samples for Flow Injection Analysis and ICP. Measured Total Bacterial Counts , pH, EC, and titrations on water samples."
 		},
 		{
 			"employer": "Food Safety Net Services",
 			"title": "Lab Technician",
 			"location": "Fresno,CA",
 			"date": "2/14-9-14",
-			"description": "Prepared and set up samples for microbiological analysis. Performed serial dilutions and used them for microbiological plating of samples. Enriched samples with appropriate media for incubation."
+			"description": "Verified and logged in client profile and microbiological/chemical test information into LIMS. Performed serial dilutions and used them for microbiological plating of samples"
 		},
 		{
 			"employer": "California State University, Fresno",
 			"title": "Student Researcher",
 			"location": "Fresno,CA",
 			"date": "08/09-08/10",
-			"description": "Synthesized gold nanoparticles for research. Performed chemical dilution experiments to aid in research of the properties of gold nanoparticles. Used a UV-spectrophotometer to measure the changes in absorption peaks of UV light by gold nanoparticles when they interacted with various salts and solvents. Gathered and organized the research data using Excel graphs to display any patterns in absorbance ratios. Properly contained and disposed of chemical waste."
+			"description": "Performed chemical dilution experiments to aid in research of the properties of gold nanoparticles. Organized data into Excel and used graphs to display any patterns in absorbance ratios."
 		}
 	]
 };
@@ -62,15 +69,33 @@ var projects = {
 	"projects": [
 		{
 			"title": "My Portfolio Web Page",
-			"date": "05/06/15-06/02/15",
+			"date": "05/15-06/15",
 			"description": "This was my first project I worked on in the Udacity Nanodegree program for Front-end Web Development. In this project, I created a webpage that would serve as my portfolio website for potential employers to see.",
 			"images": ["images/project1.jpg"]
 		},
 		{
 			"title": "My Online Resume",
-			"date": "06/03/15-Present",
+			"date": "06/15-07/15",
 			"description": "This was my second project I worked on in the Udacity Nanodegree program. I learned and utilized both javascript and jQuery to create objects for an online interactive resume.",
 			"images": ["images/project2final.jpg"]
+		},
+		{
+			"title": "Classic Arcade Game",
+			"date": "07/15-09/15",
+			"description": "This was my third project I worked on in the Udacity Nanodegree. In this project, I used object-oriented Javascript with a provided game engine to develop a tile-based arcade game loosely based on the game Frogger. Although this project exceeded Udacity's project requirements, I plan to add two additional levels in the future.",
+			"images": ["images/P3.jpg"]
+		},
+		{
+			"title": "Website Optimization Project",
+			"date": "10/15-12/15",
+			"description": "My 4th project for the Udacity Front End Nanodegree program. In this project I took an existing website and optimized it using Google's Pagespeed Insights and also made the loading time faster than 60fps. I used Node.js with Grunt to minimize CSS and also compressed the image sizes to make the site load faster.",
+			"images": ["images/P4.jpg"]
+		},
+		{
+			"title": "Google Maps Project",
+			"date": "10/15-12/15",
+			"description": "My 5th project for the Udacity FEND program. In this project I'm developing a web app that displays 5 Awesome Chinese Restaurants in San Francisco, CA. In this project, the goal is to create a webapp using Google Maps API to create a map displaying five locations of interest (I have chosen to use five highly reviewed chinese restaurants in San Francisco, CA), along with a listview (using knockout.js), a search filter, and the use of a third party API. In my project, I will be using Yelp's API to display reviews/ratings of the restaurants that will display both in the listview and in the location marker infowindows on the map. This is still a work in progress.",
+			"images": ["images/Map.jpg"]
 		}
 	]
 };
